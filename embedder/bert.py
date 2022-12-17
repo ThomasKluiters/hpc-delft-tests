f1022ebd54e67940e82477556c03833ea1930cf4
thoma <thomas.kluiters@gmail.com>
2022-12-17 22:10:51 +0100

    Add BERT Encoder

--- embedder/bert.py
+++ embedder/bert.py
@@ -1,5 +1,36 @@
+import dataclasses
+from typing import List
+
+from proteinbert import load_pretrained_model, PretrainingModelGenerator, InputEncoder
+from proteinbert.conv_and_global_attention_model import get_model_with_hidden_layers_as_outputs
+
 from core import Embedder
 
 
+@dataclasses.dataclass
 class ProteinBertEmbedder(Embedder):
-    pass
+    model_generator: PretrainingModelGenerator
+    input_encoder: InputEncoder
+
+    @classmethod
+    def load_model(cls):
+        (generator, encoder) = load_pretrained_model()
+        return cls(generator, encoder)
+
+    def compute_embeddings(self, sequences: List[str]):
+        length = max(map(len, sequences)) + 2
+        model = self.model_generator.create_model(length)
+        model = get_model_with_hidden_layers_as_outputs(model)
+
+        local_representations, _ = model.predict(self.input_encoder.encode_X(sequences, length))
+        return local_representations.mean(axis=1)
+
+    def compute_embedding(self, sequence: str):
+        return self.compute_embeddings([sequence])
+
+
+if __name__ == '__main__':
+    embedder = ProteinBertEmbedder.load_model()
+    sequence = "MDDADPEERNYDNMLKMLSDLNKDLEKLLEEMEKISVQATWMAYDMVVMRTNPTLAESMRRLEDAFVNCKEEMEKNWQELLHETKQRL"
+    embedding = embedder.compute_embeddings([sequence] * 5)
+    print(type(embedding))
--- embedder/core.py
+++ embedder/core.py
@@ -1,3 +1,10 @@
+import numpy as np
+from typing import List
+
+
 class Embedder:
-    def compute_embeddings(self):
+    def compute_embeddings(self, sequences: List[str]) -> np.ndarray:
+        raise NotImplementedError
+
+    def compute_embedding(self, sequence: str) -> np.ndarray:
         raise NotImplementedError
