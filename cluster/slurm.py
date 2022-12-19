9921668a02f7f12f02cd4edb143e2ddad8de4546
thomas <thomas.kluiters@gmail.com>
2022-12-19 17:39:14 +0100

    Fix module call

--- cluster/slurm.py
+++ cluster/slurm.py
@@ -3,5 +3,5 @@ import subprocess
 
 
 def ensure_cuda_modules_loaded():
-    subprocess.call("module use /opt/insy/modulefiles")
-    subprocess.call("module load cuda/11.2 cudnn/11.2-8.1.1.33")
+    subprocess.call("module use /opt/insy/modulefiles".split(" "))
+    subprocess.call("module load cuda/11.2 cudnn/11.2-8.1.1.33".split(" "))
