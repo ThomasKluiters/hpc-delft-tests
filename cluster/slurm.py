878f6ab84a47d1c58004bbb44df5a50a15d62878
thomas <thomas.kluiters@gmail.com>
2022-12-19 17:35:54 +0100

    Move ensure load

--- cluster/slurm.py
+++ cluster/slurm.py
@@ -1,6 +1,7 @@
 import os
+import subprocess
 
 
 def ensure_cuda_modules_loaded():
-    os.system("module use /opt/insy/modulefiles")
-    os.system("module load cuda/11.2 cudnn/11.2-8.1.1.33")
+    subprocess.call("module use /opt/insy/modulefiles")
+    subprocess.call("module load cuda/11.2 cudnn/11.2-8.1.1.33")
